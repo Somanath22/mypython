434082797e9a8c84ad9e8ab0dba585abc9ae180f
Somanath22 <somanathk2021@gmail.com>
2025-12-28 14:39:53 +0800

    added lesson1.py and homework1.py

--- day1.py
+++ day1.py
@@ -1,3 +1,21 @@
-print("Hello, World!")
-print ("hello, python")
-print ("welcome to python programing")
+# print("Hello, World!")
+# print ("hello, python")
+# print ("welcome to python programing")
+# print("Hello, World!")
+# print("I am learning Python!")
+
+# name = "somanathan"
+# age = 25
+# print ("my name is ", name)
+# print ("my age is ", age)
+
+name = "somanathan"
+age = 46
+height = 5.8
+favorite_car = "BMW"
+
+
+print ("my name is ", name)
+print ("my age is ", age)
+print ("mu hright is ", height)
+print ("my favorite car is ", favorite_car)
---
+++ homework1.py
@@ -0,0 +1,9 @@
+name = "somanathan"
+age = 46
+job = "manager"
+city = "chennai"
+
+print ("my name is ", name)
+print ("my age is ",age)
+print ("my job is", job)
+print (" my city is ", city)
