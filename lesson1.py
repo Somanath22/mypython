bcca8608c70901f36a393d3dff545583bb5ce2a3
Somanath22 <somanathk2021@gmail.com>
2025-12-28 14:53:35 +0800

    updated all the file

--- lesson1.py
+++ lesson1.py
@@ -1,10 +1,10 @@
-name = "somanathan"
-age = 46
-height = 5.8
-favorite_car = "BMW"
+# name = "somanathan"
+# age = 46
+# height = 5.8
+# favorite_car = "BMW"
 
 
-print ("my name is ", name)
-print ("my age is ", age)
-print ("my height is ", height)
-print ("my favorite car is ", favorite_car)
+# print ("my name is ", name)
+# print ("my age is ", age)
+# print ("my height is ", height)
+# print ("my favorite car is ", favorite_car)
