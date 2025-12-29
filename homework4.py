efe5e5d9f74bc656a8752b6577f5dc0011749719
Somanath22 <somanathk2021@gmail.com>
2025-12-29 16:42:27 +0800

    added lesson4, homework4, todolist

---
+++ homework4.py
@@ -0,0 +1,7 @@
+foods = []
+for i in range(5):
+    food = input("Enter your favorite food:")
+    foods.append(food)
+    print ("\n  your favorite foods are:")
+    print (foods)
+    
---
+++ lesson4.py
@@ -0,0 +1,26 @@
+cars = ["BMW", "Toyota", "Audi"]
+print(cars)
+
+cars = ["BMW", "Toyota", "Audi"]
+
+print(cars[0])  # BMW
+print(cars[1])  # Toyota
+print(cars[2])  # Audi
+
+cars = ["BMW", "Toyota"] #append method
+cars.append("Honda")
+print(cars)
+
+cars = ["BMW", "Toyota", "Honda"] # remove method
+cars.remove("Toyota")
+print(cars)
+
+cars = ["Bmw", "toyota", "audi"]
+print("My Cars List:", cars)
+print ("First Car:", cars[0])
+print ("Second Car:", cars[1])
+
+cars.append("Honda")
+print("after adding honda:", cars)
+cars.remove("toyota")
+print("after removing toyota:", cars)
