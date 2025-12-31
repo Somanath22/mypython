2af0d9b604ffd0579e2974463468abf63b544fa5
Somanath22 <somanathk2021@gmail.com>
2025-12-31 14:48:01 +0800

    updated day1 file

--- day1.py
+++ day1.py
@@ -9,13 +9,28 @@
 # print ("my name is ", name)
 # print ("my age is ", age)
 
-name = "somanathan"
-age = 46
-height = 5.8
-favorite_car = "BMW"
+# name = "somanathan"
+# age = 46
+# height = 5.8
+# favorite_car = "BMW"
 
 
-print ("my name is ", name)
-print ("my age is ", age)
-print ("mu hright is ", height)
-print ("my favorite car is ", favorite_car)
+# print ("my name is ", name)
+# print ("my age is ", age)
+# print ("mu hright is ", height)
+# print ("my favorite car is ", favorite_car)
+
+# print("Welcome to the Band name generator") # Band name generator
+# city = input("Enter the city you grew up in: ")
+# pet = input("Enter your pet's name: ")  
+# print("Your band name could be " + city + " " + pet)
+
+print ('hello'[4]) 
+
+print ('hello'[-1])  # if used to -1 it will print the last letter
+
+
+
+
+
+
