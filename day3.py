e6afe7f461c09d73f1a2a65b9a6bb0f1b5942e51
Somanath22 <somanathk2021@gmail.com>
2025-12-31 17:57:09 +0800

    day3 file added

---
+++ day3.py
@@ -0,0 +1,6 @@
+# to check odd or even
+number_to_check = int(input("what is the number to check odd or even?:"))
+if number_to_check % 2 == 0:
+    print(number_to_check, "is EVEN.")
+else:
+    print(number_to_check, "is ODD.")
