8586cd312c1d05ee60193e8c018b8409b5ea3cd1
Somanath22 <somanathk2021@gmail.com>
2026-01-01 16:01:48 +0800

    updated day3 file

--- day3.py
+++ day3.py
@@ -1,6 +1,27 @@
 # to check odd or even
-number_to_check = int(input("what is the number to check odd or even?:"))
-if number_to_check % 2 == 0:
-    print(number_to_check, "is EVEN.")
+# number_to_check = int(input("what is the number to check odd or even?:"))
+# if number_to_check % 2 == 0:
+#     print(number_to_check, "is EVEN.")
+# else:
+#     print(number_to_check, "is ODD.")
+
+# to check leap year
+# year = int(input("Enter a year to check if it's a leap year: "))
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print(year, "is a leap year.")
+# else:
+#     print(year, "is not a leap year.")  
+# #surya and magna varanam aayoram    
+# magna = input("is magna dead or alive?:")
+# if magna == "dead":
+#     print("surya meets magna")
+# else:
+#     print("surya wed magna")
+# ticket price based on age
+age = int(input("Enter your age: "))
+if age < 3:
+    print("Your ticket is free.")
+elif age <= 12:
+    print("Your ticket price is $10.")
 else:
-    print(number_to_check, "is ODD.")
+    print("Your ticket price is $15.")  
